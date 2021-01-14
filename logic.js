b69a5b7a0f8cd6b9dab3c65439d7f920f59e00f4
Joseph Rhines <jprhines@gmail.com>
2021-01-13 23:21:13 -0600

    I added the code to grab the current city, temperature, humidity, and wind speed information from the api to push to the p tags within the html when the search button is clicked

--- logic.js
+++ logic.js
@@ -1,22 +1,18 @@
-console.log("hello")
-
-
-
-
-
 $("#localButton").on("click", function(event) {
     event.preventDefault();
-    var cityLocal = $("#search-local").val();
-    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + cityLocal + "&appid=f2980ecc35d520148f7dd373eaa5ca70"
+    var cityLocal = $("#search-local").val().trim();
+    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + cityLocal + "&appid=f2980ecc35d520148f7dd373eaa5ca70&units=imperial"
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
        console.log(response);
+       $("#current").text(response.name);
+       $("#temp").text("Temperature: " + response.main.temp + " F")
+       $("#humid").text("Humidity: " + response.main.humidity + " %")
+       $("#wind").text("Wind Speed: " + response.wind.speed + " MPH")
+       console.log(response.name)
     });
-
-
-
 
 })
 
